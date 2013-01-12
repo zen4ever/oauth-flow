437d5a840a845bedd211c4dcf0e6a4492843c205
Andrii Kurinnyi <andrew@marpasoft.com>
2013-01-12 03:16:17 -0800

    Include LICENSE into distribution

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name='oauth-flow',
-      version='1.0.2',
+      version='1.0.3',
       description='Authenticate and make calls to OAuth 1.0, OAuth 2.0 services',
       author='Andrii Kurinnyi',
       author_email='andrew@zen4ever.com',
