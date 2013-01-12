e741a869af3dbe3f2df045d04f50cc38dab998a1
Andrii Kurinnyi <andrew@marpasoft.com>
2013-01-12 03:01:07 -0800

    Updated setup.py to use setuptools, and to properly utilize install_requires

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 import os
 
 setup(name='oauth-flow',
-      version='1.0',
+      version='1.0.1',
       description='Authenticate and make calls to OAuth 1.0, OAuth 2.0 services',
       author='Andrii Kurinnyi',
       author_email='andrew@zen4ever.com',
       url='https://github.com/zen4ever/oauth-flow',
-      packages=['oauth_flow', 'oauth_flow.handlers', 'oauth_flow.utils',],
+      packages=find_packages(),
       keywords=['oauth', 'twitter', 'facebook'],
       classifiers=[
           'Development Status :: 1 - Planning',
@@ -18,7 +18,7 @@ setup(name='oauth-flow',
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
       ],
-      requires=['httplib2', 'oauth2'],
+      install_requires=['httplib2', 'oauth2'],
       long_description=open(
           os.path.join(os.path.dirname(__file__), 'README.rst'),
       ).read().strip(),
